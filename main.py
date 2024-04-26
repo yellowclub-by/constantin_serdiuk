4d5fe3d9dd93c5d786070f7d3b5a7ecfe2927536
yellowclub-by <165181476+yellowclub-by@users.noreply.github.com>
2024-04-26 13:34:15 +0300

    Lesson 1 - Первый эхо-бот

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Мой первый проект на Python")
+import asyncio
+from aiogram import types
+from aiogram import Bot, Dispatcher
+from aiogram.filters import CommandStart
+
+# связываем код с ботом в телеграме
+bot = Bot(token='6243605704:AAGgzm-snEoUUZzI9-tNqeNJA7NvNiGSdQU')
+
+# создаем диспетчер для обработки команд
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start_cmd(message: types.Message):
+    await message.answer("Привет! Это бот про персонажей Brawl Stars!")
+
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer("Бот находится в разработке.\nПока он может только повторять за тобой.")
+    user_text = message.text
+    await message.answer(user_text)
+
+
+# функция для старта бота
+async def main():
+    print("Бот запущен")
+    await dp.start_polling(bot)
+
+asyncio.run(main())
+
